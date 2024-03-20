96e27a949e7b55c812b304587b2241382b37f793
kaloposia <kalopsiacraft@gmail.com>
2024-03-20 23:39:24 +0000

    Login function

--- src/test/java/com/sprint2_Sample/step_definitions/LoginStepDefs.java
+++ src/test/java/com/sprint2_Sample/step_definitions/LoginStepDefs.java
@@ -2,43 +2,27 @@ package com.sprint2_Sample.step_definitions;
 
 import com.sprint2_Sample.pages.LoginPage;
 import com.sprint2_Sample.utilities.ConfigurationReader;
+import com.sprint2_Sample.utilities.Driver;
 import io.cucumber.java.en.Given;
-public class LoginStepDefs {
 
+public class LoginStepDefs {
 
     @Given("the user is on the login page")
     public void the_user_is_on_the_login_page() {
-        System.out.println("Login to app in Before method");
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
     }
 
     @Given("the user logged in as {string}")
-    public void the_user_logged_in_as(String userType) {
-        //based on input enter that user information
-        String username =null;
-        String password =null;
+    public void the_user_logged_in_as(String string) {
 
-        if(userType.equalsIgnoreCase("driver")){
-            username = ConfigurationReader.getProperty("driver_username");
-            password = ConfigurationReader.getProperty("driver_password");
-        }else if(userType.equalsIgnoreCase("sales manager")){
-            username = ConfigurationReader.getProperty("sales_manager_username");
-            password = ConfigurationReader.getProperty("sales_manager_password");
-        }else if(userType.equalsIgnoreCase("store manager")){
-            username = ConfigurationReader.getProperty("store_manager_username");
-            password = ConfigurationReader.getProperty("store_manager_password");
-        }
-        //send username and password and login
-        new LoginPage().login(username,password);
     }
+
+    LoginPage loginPage = new LoginPage();
 
     @Given("the user logged in with username as {string} and password as {string}")
-    public void the_user_logged_in_with_username_as_and_password_as(String username, String password) {
-      LoginPage loginPage=new LoginPage();
-      loginPage.login(username,password);
+    public void the_user_logged_in_with_username_as_and_password_as(String userName, String password) {
+        loginPage.userName.sendKeys(ConfigurationReader.getProperty("hr_username"));
+        loginPage.password.sendKeys(ConfigurationReader.getProperty("hr_password"));
     }
-
-
-
-
 
 }

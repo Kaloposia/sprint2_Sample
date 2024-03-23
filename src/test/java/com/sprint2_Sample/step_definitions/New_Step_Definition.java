92cf8d08bb80efaa43e75af70821285fffd5390b
Musty <146491208+Musty123456@users.noreply.github.com>
2024-03-23 08:04:55 +0000

    Update New_Step_Definition.java

--- src/test/java/com/sprint2_Sample/step_definitions/New_Step_Definition.java
+++ src/test/java/com/sprint2_Sample/step_definitions/New_Step_Definition.java
@@ -3,6 +3,8 @@ package com.sprint2_Sample.step_definitions;
 public class New_Step_Definition {
     public static void main(String[] args) {
         System.out.println("Added new step definition");
+    
+    System.out.println("add more things");
     }
 
 }

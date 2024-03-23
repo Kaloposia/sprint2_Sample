85b034d7a0b684a7b18bcbcab088fa848327f608
musta <mustafaoguzhan93@gmail.com>
2024-03-23 07:55:48 +0000

    added new step definition

---
+++ src/test/java/com/sprint2_Sample/step_definitions/New_Step_Definition.java
@@ -0,0 +1,8 @@
+package com.sprint2_Sample.step_definitions;
+
+public class New_Step_Definition {
+    public static void main(String[] args) {
+        System.out.println("Added new step definition");
+    }
+
+}
